6275f26e97530f7407436e6f562c2d83a36bcd2a
JessicaDMunozO <jessitae@gmail.com>
2024-05-14 20:59:15 -0500

    Componente base página principal

---
+++ farmaya-app/src/components/MainPage/MainPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import "../../components/MainPage/MainPage.css"
+
+const MainPage = () => {
+    return (
+        <div>
+            <h1>Orders</h1>
+            <div className="card">
+                <div className="card-border-top">
+                </div>
+                <div className="img">
+                </div>
+                <span> Person</span>
+                <p className="job"> Job Title</p>
+                <button> Click
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
