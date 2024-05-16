6d4ba3cdfa2110972ff24e9ff4b641faa55a922a
JessicaDMunozO <jessitae@gmail.com>
2024-05-15 20:35:36 -0500

    Ordenes obtenidas del backend y manejo token

--- farmaya-app/src/components/MainPage/MainPage.jsx
+++ farmaya-app/src/components/MainPage/MainPage.jsx
@@ -1,20 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
 
 import "../../components/MainPage/MainPage.css"
 
 const MainPage = () => {
+    const [orders, setOrders] = useState();
+
+    const baseURL = "http://localhost:80/db/orders";
+
+    useEffect(() => {
+        const localStorageToken = localStorage.getItem("token");
+
+        if (localStorageToken) {
+            const token = JSON.parse(localStorageToken).token;
+            axios.get(baseURL, {
+                headers: {
+                    "authorization": `Bearer ${token}`
+                }
+            }).then((response) => {
+                setOrders(response.data);
+            })
+        };
+    }, []);
+
+    if (!orders) return null
+
     return (
         <div>
             <h1>Orders</h1>
-            <div className="card">
-                <div className="card-border-top">
-                </div>
-                <div className="img">
-                </div>
-                <span> Person</span>
-                <p className="job"> Job Title</p>
-                <button> Click
-                </button>
+            <div className='cards-container'>
+                {orders.map((order) => (
+                    <div className="card" key={order.order_id}>
+                        <div className="card-border-top">
+                        </div>
+                        <span>Patient {order.patient_name}</span>
+                        <p className="document"> Document: {order.patient_document}</p>
+                        <p className="address"> Address: {order.address}</p>
+                        {order.delivered ? <p className="delivered">State: delivered</p> : <p className="delivered">State: pending</p>}
+                        <p className="doctor"> Doctor: {order.doctor_name}</p>
+                        {order.domicile ? <p className="domicile">Domicile: Yes</p> : <p className="domicile">Domicile: No</p>}
+                        <p className="provider"> Provider: {order.provider}</p>
+                        <p className="medicines"> Medicines: </p>
+                        <ul>
+                            {order.medicines.map((medicine, i) => (
+                                <li className="medicine" key={i}>{medicine.medicine_name}: {medicine.amount}</li>
+                            ))}
+                        </ul>
+                        <button> Delivered
+                        </button>
+                    </div>
+                ))}
             </div>
         </div>
     );
