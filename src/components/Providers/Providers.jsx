9578f0c59d1c0b77bb8507b350d8fa21109062e8
JessicaDMunozO <jessitae@gmail.com>
2024-05-23 13:30:27 -0500

    Estilos botón y buscador

--- src/components/Providers/Providers.jsx
+++ src/components/Providers/Providers.jsx
@@ -58,8 +58,19 @@ const Providers = () => {
     return (
         <div>
             <h1>Providers</h1>
-            <button className='addProvider_button' onClick={handleAddProvider}>Add provider</button>
-            <input type="text" value={search} onChange={handleSearchChange} placeholder="providers or medicines... " />
+            <button className='addProvider_button' onClick={handleAddProvider} disabled={role === "doctor"}>
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"></path></svg> Add provider
+                </span>
+            </button>
+            <div className="group">
+                <svg className="icon" aria-hidden="true" viewBox="0 0 24 24">
+                    <g>
+                        <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+                    </g>
+                </svg>
+                <input placeholder="Providers or medicines... " type="search" className="input_search" value={search} onChange={handleSearchChange} />
+            </div>
             <div className='cards-container'>
                 {providers.map((provider) => (
                     <div className="card" key={provider._id}>
