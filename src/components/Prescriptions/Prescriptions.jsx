ec6465500c98a749f5e7730df70f4754228e9a9e
Ricardo-Olarte <olarte.jose.r@gmail.com>
2024-05-24 10:58:29 -0500

    Corrección visualización

--- src/components/Prescriptions/Prescriptions.jsx
+++ src/components/Prescriptions/Prescriptions.jsx
@@ -85,7 +85,17 @@ const Prescriptions = () => {
         event.preventDefault();
         const token = JSON.parse(localStorageToken).token;
         const baseURL = "http://localhost:80/db/orders/new";
-        const json = JSON.stringify({ "medicine_name": medicine_name, "amount": amount, "authorization_required": authorization_required, "domicile": domicile,  "address": address, "provider": provider,  "doctor_name": doctor_name, "patient_document": patient_document,  "patient_name": patient_name});
+        const json = JSON.stringify({ 
+            "medicine_name": medicine_name, 
+            "amount": amount, 
+            "authorization_required": authorization_required, 
+            "domicile": domicile,  
+            "address": address, 
+            "provider": provider,  
+            "doctor_name": doctor_name, 
+            "patient_document": patient_document,  
+            "patient_name": patient_name
+        });
 
         try {
             await axios.post(baseURL, json, {
@@ -105,47 +115,47 @@ const Prescriptions = () => {
             <form className="form" onSubmit={handleSubmit}>
                 <p className="title">New Prescription test</p>
                 <label>
-                    <input className="input" type="number" required value={medicineQuantity} onChange={handleMedicineQuantityChange} />
+                    <input className="input" type="number" required="" value={medicineQuantity} onChange={handleMedicineQuantityChange} />
                     <span>quantity</span><br></br>
                 </label>
                 {medicines.map((medicine, index) => (
                     <div key={index}>
                         <label>
-                            <input className="input" type="text" required value={medicine.medicine_name} onChange={(e) => handleMedicineChange(index, 'medicine_name', e.target.value)} />
+                            <input className="input" type="text" required="" value={medicine.medicine_name} onChange={(e) => handleMedicineChange(index, 'medicine_name', e.target.value)} />
                             <span>medicine_name</span><br></br>
                         </label>
                         <label>
-                            <input className="input" type="text" required value={medicine.amount} onChange={(e) => handleMedicineChange(index, 'amount', e.target.value)} />
+                            <input className="input" type="text" required="" value={medicine.amount} onChange={(e) => handleMedicineChange(index, 'amount', e.target.value)} />
                             <span>amount</span><br></br>
                         </label>
                         <label>
-                            <input className="input" type="text" required value={medicine.authorization_required} onChange={(e) => handleMedicineChange(index, 'authorization_required', e.target.value)} />
+                            <input className="input" type="text" required="" value={medicine.authorization_required} onChange={(e) => handleMedicineChange(index, 'authorization_required', e.target.value)} />
                             <span>authorization_required</span><br></br>
                         </label>
                     </div>
                 ))}
                 <label>
-                    <input className="input" type="text" required value={domicile} onChange={handleDomicileChange} />
+                    <input className="input" type="text" required="" value={domicile} onChange={handleDomicileChange} />
                     <span>domicile</span><br></br>
                 </label>
                 <label>
-                    <input className="input" type="text" required value={address} onChange={handleAddressChange} />
+                    <input className="input" type="text" required="" value={address} onChange={handleAddressChange} />
                     <span>address</span><br></br>
                 </label>
                 <label>
-                    <input className="input" type="text" required value={provider} onChange={handleProviderChange} />
+                    <input className="input" type="text" required="" value={provider} onChange={handleProviderChange} />
                     <span>provider</span><br></br>
                 </label>
                 <label>
-                    <input className="input" type="text" required value={doctor_name} onChange={handleDoctorNameChange} />
+                    <input className="input" type="text" required="" value={doctor_name} onChange={handleDoctorNameChange} />
                     <span>doctor_name</span><br></br>
                 </label>
                 <label>
-                    <input className="input" type="text" required value={patient_document} onChange={handlePatientDocumentChange} />
+                    <input className="input" type="text" required="" value={patient_document} onChange={handlePatientDocumentChange} />
                     <span>patient_document</span><br></br>
                 </label>
                 <label>
-                    <input className="input" type="text" required value={patient_name} onChange={handlePatientNameChange} />
+                    <input className="input" type="text" required="" value={patient_name} onChange={handlePatientNameChange} />
                     <span>patient_name</span><br></br>
                 </label>
                 <button className="submit">Add medicine</button>
